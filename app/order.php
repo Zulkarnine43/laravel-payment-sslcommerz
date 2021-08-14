28f8858ec8991263327310433b14d223fe86281e
Zulkar Nine <zulkarnine43@gmail.com>
2021-08-14 13:02:25 +0600

    laravel-sslcommerz-payment

---
+++ app/order.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class order extends Model
+{
+    //
+
+    protected $fillable=['name','email','phone','amount','address','status','transaction_id','currency'];
+}
---
+++ database/migrations/2021_08_14_061847_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('amount');
+            $table->string('address');
+            $table->string('status');
+            $table->string('transaction_id');
+            $table->string('currency');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
